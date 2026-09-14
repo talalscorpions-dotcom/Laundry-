f4cb82478914e364768c718f12dfeb7922e7ace6
talalscorpions-dotcom <talalscorpions-dotcom@users.noreply.github.com>
2026-09-14 10:29:26 +0000

    deploy: c3eabfb51706fbca1936230ff09d44edcfab9df5

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "594141549"
+    serviceWorkerVersion: "1517562830"
   }
 });
